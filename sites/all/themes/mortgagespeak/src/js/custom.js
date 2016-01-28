9689f9f2f44ef538806fbbec2b56ea6eb051ab5b
vivek-addweb <vivek.addweb@gmail.com>
2016-01-28 13:51:33 +0530

    Design: changes top border color like Gmail tab design

--- sites/all/themes/mortgagespeak/src/js/custom.js
+++ sites/all/themes/mortgagespeak/src/js/custom.js
@@ -1,22 +1,28 @@
 jQuery(document).ready(function() {
-  /* JS Code for Custom checkbox design */
+
+  // JS Code for Custom checkbox design
   jQuery('input[type="radio"], input[type="checkbox"]').wrap('<div class="input-rc"></div>');
   jQuery('.input-rc').append('<span class="input-rc-span"></span>');
-  /* End */
-  
+  // End
+
+  // Add class in form pages
   var loc = window.location.pathname;
   if(loc == '/user'){
     jQuery('.tabs--primary').find('a[href="/user/register"]').addClass('hide-register');
     jQuery('.tabs--primary').find('a[href="/user/password"]').addClass('hide-pass');
   }
+  // End
 
+  // Select all
   jQuery('#edit-select-all--2').change(function(){
-  var check = this.checked;
-  if(check == true){
-     jQuery('.field-type-taxonomy-term-reference #edit-field-news-topics-und--2 .form-type-checkbox .form-checkbox').prop('checked', true);  
-  }
-  else{
-     jQuery('.field-type-taxonomy-term-reference #edit-field-news-topics-und--2 .form-type-checkbox .form-checkbox').prop('checked', false);
-  }
+    var check = this.checked;
+    if(check == true){
+       jQuery('.field-type-taxonomy-term-reference #edit-field-news-topics-und--2 .form-type-checkbox .form-checkbox').prop('checked', true);  
+    }
+    else{
+       jQuery('.field-type-taxonomy-term-reference #edit-field-news-topics-und--2 .form-type-checkbox .form-checkbox').prop('checked', false);
+    }
   });
+  // End
+
 });
