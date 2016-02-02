d190388adf90472b6bd9435255486e07e93446ef
saurabhd <saurabhdhariwal.com@gmail.com>
2016-02-02 18:33:58 +0530

    Design: Change in css and js for popup design

--- sites/all/themes/mortgagespeak/src/js/custom.js
+++ sites/all/themes/mortgagespeak/src/js/custom.js
@@ -25,40 +25,31 @@ jQuery(document).ready(function() {
   });
   // End
 
-  jQuery(document).ready(function() {
-
-  // JS Code for Custom checkbox design
-  jQuery('input[type="radio"], input[type="checkbox"]').wrap('<div class="input-rc"></div>');
-  jQuery('.input-rc').append('<span class="input-rc-span"></span>');
-  // End
-
-  // Add class in form pages
-  var loc = window.location.pathname;
-  if(loc == '/user'){
-    jQuery('.tabs--primary').find('a[href="/user/register"]').addClass('hide-register');
-    jQuery('.tabs--primary').find('a[href="/user/password"]').addClass('hide-pass');
-  }
-  // End
-
-  // Select all
-  jQuery('#edit-select-all--2').change(function(){
-    var check = this.checked;
-    if(check == true){
-       jQuery('.field-type-taxonomy-term-reference #edit-field-news-topics-und--2 .form-type-checkbox .form-checkbox').prop('checked', true);  
-    }
-    else{
-       jQuery('.field-type-taxonomy-term-reference #edit-field-news-topics-und--2 .form-type-checkbox .form-checkbox').prop('checked', false);
-    }
-  });
-  // End
-
   jQuery('.page-my-page-tracked-companies ul.menu li:nth-child(2)').addClass('active-trail active');
   jQuery('.page-my-page-tracked-companies ul.menu li:nth-child(2) a').addClass('active-trail active');
   jQuery('.page-my-page-saved-articles ul.menu li:nth-child(2)').addClass('active-trail active');
   jQuery('.page-my-page-saved-articles ul.menu li:nth-child(2) a').addClass('active-trail active');
 
-  
+  /* Popup design */
+    jQuery('.content-lists .content-list .views-field-title').click(function() {
+      if(jQuery(this).parent().children('.content-lists .content-list .views-popup-container').hasClass('open-popup')) {
+          jQuery(this).parent().children('.open-popup').removeClass('open-popup');
+      }
+      else {
+        jQuery('.content-lists .content-list .views-popup-container').removeClass('open-popup');
+        jQuery(this).parent().children('.content-lists .content-list .views-popup-container').addClass('open-popup');
+      }
+    });
 
+    jQuery('.views-popup-container .popup-header .close-popup').click(function() {
+      jQuery('.content-lists .content-list .views-popup-container').removeClass('open-popup');
+    });
+
+      jQuery(document).keydown(function(e) {
+      if (e.keyCode == 27) {
+          /*window.close();*/
+          jQuery('.content-lists .content-list .views-popup-container').removeClass('open-popup');
+      }
 });
-
+  /* End */
 });
--- sites/all/themes/mortgagespeak/templates/views-view-unformatted--popup-views--block-1.tpl.php
+++ sites/all/themes/mortgagespeak/templates/views-view-unformatted--popup-views--block-1.tpl.php
@@ -23,9 +23,9 @@
 <div class="popup-header">
 	<div class="popup-title">Excerpt Time-Saver</div>
 	<div class="tooltips">
-		<div>What's this?</div>
+		<div>( What's this? )</div>
 	</div>
-	<a href="#">Close</a>
+	<a href="#" class="close-popup">x</a>
 </div>
 	<?php foreach ($rows as $id => $row): 
 		$title = $row['title'];
@@ -47,39 +47,41 @@
 				<a href="<?php print $url;?>" target="_blank"><?php print $title; ?>
 				</a>
 			</div> 
-			<div class="views-field views-field-body">
-			    <span class="field-content">
-			    	<?php print $row['field_pop_up']; ?>
-			    </span>
+
+			<div class="views-field views-field-sharethis">
+				<div class="sharethis-content">
+				  <span class="field-content">
+					  <div class="sharethis-wrapper">
+
+							<span class="chicklets linkedin">
+								<a href="http://www.linkedin.com/shareArticle?mini=true&url=<?php print $url; ?>&summary=<?php print $body; ?>" class="linkedinshare"><img src="/sites/all/themes/mortgage_new_theme/images/link.png"/></a>
+							</span>
+
+							<span class="chicklets facebook">
+								<a href='http://www.facebook.com/sharer.php?u=<?php print $url; ?>&t=<?php echo $title; ?>' class="facebook"><img src="/sites/all/themes/mortgage_new_theme/images/facebook1.png" /></a>
+							</span>
+
+							<span class="chicklets twitter">
+								<a href="https://twitter.com/share?&text=<?php print $strFinal; ?>&url=<?php print $url; ?>" class="twitter-share-button" data-via="MortgageSpeak" data-count="none"><img src="/sites/all/themes/mortgage_new_theme/images/twitter1.png" /></a>
+							</span>
+
+							<span class="chicklets googleplus">
+								<a href="https://plus.google.com/share?url=<?php print ($url); ?>&body=<?php print $body; ?>" class="google"><img src="/sites/all/themes/mortgage_new_theme/images/google1.png" /></a>
+							</span>
+
+							<span class="chicklets email">
+								<a href='mailto:Article@MortgageSpeak.com?subject=<?php echo $title; ?>&body=I wanted to share this article with you: <?php print ($url. '     ' .$body); ?>' class="email" target="_blank"><img src="/sites/all/themes/mortgage_new_theme/images/mail1.png" /></a>
+							</span>
+						</div> 
+						 <!-- Flag link-->
+						 <?php print $row['ops'];?>
+					</span>
+				</div>
 			</div>
-			<div class="views-field views-field-sharethis">    
-			  <span class="views-label views-label-sharethis">More: </span>    
-			  <span class="field-content">
-				  <div class="sharethis-wrapper">
-
-						<span class="chicklets linkedin">
-							<a href="http://www.linkedin.com/shareArticle?mini=true&url=<?php print $url; ?>&summary=<?php print $body; ?>" class="linkedinshare"><img src="/sites/all/themes/mortgage_new_theme/images/link.png"/></a>
-						</span>
-
-						<span class="chicklets facebook">
-							<a href='http://www.facebook.com/sharer.php?u=<?php print $url; ?>&t=<?php echo $title; ?>' class="facebook"><img src="/sites/all/themes/mortgage_new_theme/images/facebook1.png" /></a>
-						</span>
-
-						<span class="chicklets twitter">
-							<a href="https://twitter.com/share?&text=<?php print $strFinal; ?>&url=<?php print $url; ?>" class="twitter-share-button" data-via="MortgageSpeak" data-count="none"><img src="/sites/all/themes/mortgage_new_theme/images/twitter1.png" /></a>
-						</span>
-
-						<span class="chicklets googleplus">
-							<a href="https://plus.google.com/share?url=<?php print ($url); ?>&body=<?php print $body; ?>" class="google"><img src="/sites/all/themes/mortgage_new_theme/images/google1.png" /></a>
-						</span>
-
-						<span class="chicklets email">
-							<a href='mailto:Article@MortgageSpeak.com?subject=<?php echo $title; ?>&body=I wanted to share this article with you: <?php print ($url. '     ' .$body); ?>' class="email" target="_blank"><img src="/sites/all/themes/mortgage_new_theme/images/mail1.png" /></a>
-						</span>
-					</div> 
-					 <!-- Flag link-->
-					 <?php print $row['ops'];?>
-				</span>  
+			<div class="views-field views-field-body">
+				<span class="field-content quote">
+					<?php print $row['field_pop_up']; ?>
+				</span>
 			</div> 
 			<div class="custom-ads"><?php print render($block['content']); ?></div>
 	  </div>
