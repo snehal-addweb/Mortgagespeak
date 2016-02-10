70d721a800c6fbbcf88efca6049f7d718f4b2c64
Roshni Patel <roshnip.addweb@gmail.com>
2016-02-10 18:27:59 +0530

    upload text changes

--- sites/all/themes/mortgagespeak/js/custom.js
+++ sites/all/themes/mortgagespeak/js/custom.js
@@ -1,1 +1,71 @@
-jQuery(document).ready(function(){jQuery('input[type="radio"], input[type="checkbox"]').wrap('<div class="input-rc"></div>'),jQuery(".input-rc").append('<span class="input-rc-span"></span>');var e=window.location.pathname;"/user"==e&&(jQuery(".tabs--primary").find('a[href="/user/register"]').addClass("hide-register"),jQuery(".tabs--primary").find('a[href="/user/password"]').addClass("hide-pass")),jQuery("#edit-select-all--2").change(function(){var e=this.checked;1==e?jQuery(".field-type-taxonomy-term-reference #edit-field-news-topics-und--2 .form-type-checkbox .form-checkbox").prop("checked",!0):jQuery(".field-type-taxonomy-term-reference #edit-field-news-topics-und--2 .form-type-checkbox .form-checkbox").prop("checked",!1)}),jQuery(".page-my-page-tracked-companies ul.menu li:nth-child(2)").addClass("active-trail active"),jQuery(".page-my-page-tracked-companies ul.menu li:nth-child(2) a").addClass("active-trail active"),jQuery(".page-my-page-saved-articles ul.menu li:nth-child(2)").addClass("active-trail active"),jQuery(".page-my-page-saved-articles ul.menu li:nth-child(2) a").addClass("active-trail active"),jQuery(".content-lists .content-list .views-field-title").click(function(){jQuery(this).parent().children(".content-lists .content-list .views-popup-container").hasClass("open-popup")?jQuery(this).parent().children(".open-popup").removeClass("open-popup"):(jQuery(".content-lists .content-list .views-popup-container").removeClass("open-popup"),jQuery(this).parent().children(".content-lists .content-list .views-popup-container").addClass("open-popup"))}),jQuery(".views-popup-container .popup-header .close-popup").click(function(){jQuery(".content-lists .content-list .views-popup-container").removeClass("open-popup")}),jQuery(document).keydown(function(e){27==e.keyCode&&jQuery(".content-lists .content-list .views-popup-container").removeClass("open-popup")}),jQuery('input[type="file"]').wrap('<div class="input-file"><div class="input-file-sub"></div></div>'),jQuery(".input-file").append('<span class="input-file-name">No file chosen</span>'),jQuery(".input-file-sub input").change(function(){var e=jQuery(this).val();jQuery(this).parent().parent().children(".input-file-name").text(e)})});
+jQuery(document).ready(function() {
+
+	  // JS Code for Custom checkbox design
+  jQuery('input[type="radio"], input[type="checkbox"]').wrap('<div class="input-rc"></div>');
+  jQuery('.input-rc').append('<span class="input-rc-span"></span>');
+  // End
+
+  // Add class in form pages
+  var loc = window.location.pathname;
+  if(loc == '/user'){
+    jQuery('.tabs--primary').find('a[href="/user/register"]').addClass('hide-register');
+    jQuery('.tabs--primary').find('a[href="/user/password"]').addClass('hide-pass');
+  }
+  // End
+
+  // Select all
+  jQuery('#edit-select-all--2').change(function(){
+    var check = this.checked;
+    if(check == true){
+       jQuery('.field-type-taxonomy-term-reference #edit-field-news-topics-und--2 .form-type-checkbox .form-checkbox').prop('checked', true);  
+    }
+    else{
+       jQuery('.field-type-taxonomy-term-reference #edit-field-news-topics-und--2 .form-type-checkbox .form-checkbox').prop('checked', false);
+    }
+  });
+  // End
+
+  jQuery('.page-my-page-tracked-companies ul.menu li:nth-child(2)').addClass('active-trail active');
+  jQuery('.page-my-page-tracked-companies ul.menu li:nth-child(2) a').addClass('active-trail active');
+  jQuery('.page-my-page-saved-articles ul.menu li:nth-child(2)').addClass('active-trail active');
+  jQuery('.page-my-page-saved-articles ul.menu li:nth-child(2) a').addClass('active-trail active');
+
+  /* Popup design */
+    jQuery('.content-lists .content-list .views-field-title').click(function() {
+      if(jQuery(this).parent().children('.content-lists .content-list .views-popup-container').hasClass('open-popup')) {
+          jQuery(this).parent().children('.open-popup').removeClass('open-popup');
+      }
+      else {
+        jQuery('.content-lists .content-list .views-popup-container').removeClass('open-popup');
+        jQuery(this).parent().children('.content-lists .content-list .views-popup-container').addClass('open-popup');
+      }
+    });
+
+    jQuery('.views-popup-container .popup-header .close-popup').click(function() {
+      jQuery('.content-lists .content-list .views-popup-container').removeClass('open-popup');
+    });
+
+	jQuery(document).keydown(function(e) {
+	      if (e.keyCode == 27) {
+	          /*window.close();*/
+	          jQuery('.content-lists .content-list .views-popup-container').removeClass('open-popup');
+	      }
+	});
+  /* End */
+
+  /* Custom input type file design */
+  jQuery('input[type="file"]').wrap('<div class="input-file"><div class="input-file-sub"></div></div>');
+  jQuery('.input-file').append('<span class="input-file-name">No file chosen</span>');
+  jQuery('.input-file-sub input').change(function() {
+    var filename = jQuery(this).val();
+    jQuery(this).parent().parent().children(".input-file-name").text(filename);
+  });
+  /* End */
+
+  /* Change Upload Text in Upload webform*/
+  	jQuery('#webform-client-form-42 .attach-content .form-managed-file button').text('Upload File');
+
+
+  /* End */
+
+});
