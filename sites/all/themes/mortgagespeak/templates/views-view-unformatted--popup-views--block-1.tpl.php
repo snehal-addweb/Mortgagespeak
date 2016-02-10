0d16632ee5b8a07a0e5fc9fcdbfad58a30fe9f13
vivek-addweb <vivek.addweb@gmail.com>
2016-02-10 17:33:37 +0530

    Design: Solved issues #23 to #43

--- sites/all/themes/mortgagespeak/templates/views-view-unformatted--popup-views--block-1.tpl.php
+++ sites/all/themes/mortgagespeak/templates/views-view-unformatted--popup-views--block-1.tpl.php
@@ -50,46 +50,49 @@
 		?>
 
 	  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
-			<div class="views-field views-field views-field-title">        
-				<a href="<?php print $title_link;?>" target="_blank"><?php print $title; ?>
-				</a>
-			</div> 
+	  
+			<div class="pop-up-content">
+				<div class="views-field views-field views-field-title">        
+					<a href="<?php print $title_link;?>" target="_blank"><?php print $title; ?>
+					</a>
+				</div>
+				<div class="views-field views-field-sharethis">
+					<div class="sharethis-content">
+					  <span class="field-content">
+						  <div class="sharethis-wrapper">
 
-			<div class="views-field views-field-sharethis">
-				<div class="sharethis-content">
-				  <span class="field-content">
-					  <div class="sharethis-wrapper">
+								<span class="chicklets linkedin">
+									<a href="http://www.linkedin.com/shareArticle?mini=true&url=<?php print $url; ?>&summary=<?php print $body; ?>" target='_blank' class="linkedinshare"><img src="/sites/all/themes/mortgage_new_theme/images/link.png"/></a>
+								</span>
 
-							<span class="chicklets linkedin">
-								<a href="http://www.linkedin.com/shareArticle?mini=true&url=<?php print $url; ?>&summary=<?php print $body; ?>" target='_blank' class="linkedinshare"><img src="/sites/all/themes/mortgage_new_theme/images/link.png"/></a>
-							</span>
+								<span class="chicklets facebook">
+									<a href='http://www.facebook.com/sharer.php?u=<?php print $url; ?>&t=<?php echo $title; ?>' target='_blank' class="facebook"><img src="/sites/all/themes/mortgage_new_theme/images/facebook1.png" /></a>
+								</span>
 
-							<span class="chicklets facebook">
-								<a href='http://www.facebook.com/sharer.php?u=<?php print $url; ?>&t=<?php echo $title; ?>' target='_blank' class="facebook"><img src="/sites/all/themes/mortgage_new_theme/images/facebook1.png" /></a>
-							</span>
+								<span class="chicklets twitter">
+									<a href="https://twitter.com/share?&text=<?php print $strFinal; ?>&url=<?php print $url; ?>" target='_blank' class="twitter-share-button" data-via="MortgageSpeak" data-count="none"><img src="/sites/all/themes/mortgage_new_theme/images/twitter1.png" /></a>
+								</span>
 
-							<span class="chicklets twitter">
-								<a href="https://twitter.com/share?&text=<?php print $strFinal; ?>&url=<?php print $url; ?>" target='_blank' class="twitter-share-button" data-via="MortgageSpeak" data-count="none"><img src="/sites/all/themes/mortgage_new_theme/images/twitter1.png" /></a>
-							</span>
+								<span class="chicklets googleplus">
+									<a href="https://plus.google.com/share?url=<?php print ($url); ?>&body=<?php print $body; ?>" target='_blank' class="google"><img src="/sites/all/themes/mortgage_new_theme/images/google1.png" /></a>
+								</span>
 
-							<span class="chicklets googleplus">
-								<a href="https://plus.google.com/share?url=<?php print ($url); ?>&body=<?php print $body; ?>" target='_blank' class="google"><img src="/sites/all/themes/mortgage_new_theme/images/google1.png" /></a>
-							</span>
-
-							<span class="chicklets email">
-								<a href='mailto:Article@MortgageSpeak.com?subject=<?php echo $title; ?>&body=I wanted to share this article with you: <?php print ($url. '     ' .$body); ?>' class="email" target="_blank"><img src="/sites/all/themes/mortgage_new_theme/images/mail1.png" /></a>
-							</span>
-						</div> 
-						 <!-- Flag link-->
-						 <?php print $row['ops'];?>
+								<span class="chicklets email">
+									<a href='mailto:Article@MortgageSpeak.com?subject=<?php echo $title; ?>&body=I wanted to share this article with you: <?php print ($url. '     ' .$body); ?>' class="email" target="_blank"><img src="/sites/all/themes/mortgage_new_theme/images/mail1.png" /></a>
+								</span>
+							</div> 
+							 <!-- Flag link-->
+							 <?php print $row['ops'];?>
+						</span>
+					</div>
+				</div>
+				<div class="views-field views-field-body">
+					<span class="field-content quote">
+						<?php print $row['field_pop_up']; ?>
 					</span>
 				</div>
 			</div>
-			<div class="views-field views-field-body">
-				<span class="field-content quote">
-					<?php print $row['field_pop_up']; ?>
-				</span>
-			</div> 
+
 			<div class="custom-ads"><?php print render($block['content']); ?></div>
 	  </div>
 	<?php endforeach; ?>
