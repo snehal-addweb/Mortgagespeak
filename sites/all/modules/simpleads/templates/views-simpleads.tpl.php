47e4ab42b41e0d28fc6f719602422acb26abfc9d
Roshni Patel <roshnip.addweb@gmail.com>
2016-02-19 17:00:36 +0530

    deleted simple ads module

--- sites/all/modules/simpleads/templates/views-simpleads.tpl.php
+++ sites/all/modules/simpleads/templates/views-simpleads.tpl.php
@@ -2,11 +2,40 @@
 
 /**
  * @file
- * Default simple view template to display ads.
+ * views-simpleads.tpl.php
  */
+
 ?>
-<?php if (isset($tid) && isset($ads_list) && !empty($ads_list)) : ?>
-<div class="simpleads-<?php print $tid; ?><?php if ($prefix) :?>-<?php print $prefix; ?><?php endif; ?>">
-  <?php print $ads_list; ?>
+<?php if ( isset($rows) ) : ?>
+<div data-fx="<?php print $options['transition_effect']; ?>" data-speed="<?php print $options['speed']; ?>" class="simpleads-wrapper">
+
+  <?php if (isset($options['items_per_slide']) && $options['items_per_slide'] > 1) : ?>
+
+    <?php $chunks = array_chunk($rows, $options['items_per_slide']); ?>
+
+    <?php foreach ($chunks as $rows): ?>
+
+      <div class="simpleads-chunk-item">
+
+        <?php foreach ($rows as $id => $row): ?>
+
+          <?php print theme('simpleads_type_' . $type[$id], array('output' => $row, 'type' => $type[$id], 'entity' => $entity[$id], 'options' => $options, 'css_classes' => $classes_array[$id])); ?>
+
+        <?php endforeach; ?>
+
+      </div>
+
+    <?php endforeach; ?>
+
+  <?php else : ?>
+
+    <?php foreach ($rows as $id => $row): ?>
+
+      <?php print theme('simpleads_type_' . $type[$id], array('output' => $row, 'type' => $type[$id], 'entity' => $entity[$id], 'options' => $options, 'css_classes' => $classes_array[$id])); ?>
+
+    <?php endforeach; ?>
+
+  <?php endif; ?>
+
 </div>
 <?php endif; ?>
