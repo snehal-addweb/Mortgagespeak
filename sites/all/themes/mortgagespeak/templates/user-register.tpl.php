791482c0da0edeae845e89e10190534fab675f5d
Saurabh Dhariwal <roshnip.addweb@gmail.com>
2016-04-09 12:36:48 +0530

    reg pic module to add pic in registration form

--- sites/all/themes/mortgagespeak/templates/user-register.tpl.php
+++ sites/all/themes/mortgagespeak/templates/user-register.tpl.php
@@ -3,28 +3,42 @@ global $base_url;
 ?>
 <div class="register-container">
 <div class="register-text">Register for <span class="green-text">free</span> here! Already have an account? <a href="<?php print $base_url; ?>/user" class="blue-text">Log in here.</a></div>
-<?php
 
-print render($form['account']['name']);?>
-<div class="field-type-wrap"><?php
-print render ($form['field_first_name']);
-print render ($form['field_last_name']);
-?></div><?php
-print render($form['account']['mail']);
-print render ($form['account']['pass']);
-?>
-<span class="news-text">Choose News Topics Tracked in Your "My Page":</span>
+<div class="user-register-pic">
+  <div class="register-my-details">
+  <div class="my-details-header">My Details</div>
+  <?php
 
+    print render($form['account']['name']);?>
+    <div class="field-type-wrap"><?php
+      print render ($form['field_first_name']);
+      print render ($form['field_last_name']);
+    ?></div><?php
+      print render($form['account']['mail']);
+      print render ($form['account']['pass']);
+    ?>
+  </div>
+  <div class="user-pic-reg">
+    <?php print render($form['picture']); ?>
+  </div>
+</div>
+
+<div class="register-my-page-details">
+  <div class="my-page-details-header">"My Page" Details</div>
+  <span class="news-text">Follow these "news topics" Tracked in your custom page.</span>
+
+  <?php 
+    print render($form['select_all']);
+    print render($form['field_news_topics']);
+  ?>
+</div>
 <?php 
-print render($form['select_all']);
-print render($form['field_news_topics']);
-print render($form['captcha']);
+  print render($form['captcha']);
 
-
-print drupal_render($form['actions']);
-print drupal_render($form['form_build_id']);
-print drupal_render($form['form_id']);
-
+  print drupal_render($form['actions']);
+  print drupal_render($form['form_build_id']);
+  print drupal_render($form['form_id']);
 ?>
+
 <div class="register-bottom-text"><span>By clicking the button, you agree to our <a href="<?php print $base_url; ?>/terms-and-conditions">terms and conditions</a>.</span><span>Already have an account? <a href="<?php print $base_url; ?>/user">Log in here.</a></span></div>
 </div>
