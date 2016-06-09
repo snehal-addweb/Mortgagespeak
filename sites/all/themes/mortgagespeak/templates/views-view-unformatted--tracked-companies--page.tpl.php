551316a901bd93f2b3b8a1b7a9d7e84a3d27ddf0
roshni-addweb <roshnip.addweb@gmail.com>
2016-06-09 11:36:55 +0530

    edits for #54

--- sites/all/themes/mortgagespeak/templates/views-view-unformatted--tracked-companies--page.tpl.php
+++ sites/all/themes/mortgagespeak/templates/views-view-unformatted--tracked-companies--page.tpl.php
@@ -112,13 +112,6 @@
 	    </span>
 		</div>
 
-		<div class="views-field views-field-field-alpha-news-topics">    
-			<span class="views-label views-label-field-alpha-news-topics">Mortgage Topic:</span>    
-			<div class="field-content">
-				<?php print $row['field_alpha_news_topics']; ?>
-			</div>  
-		</div> 
-
 		<?php //if(in_array('News Intelligence', $user->roles) || in_array('administrator', $user->roles)) {?>
 			<div class="views-field views-field-field-company-tag">    
 				<span class="views-label views-label-field-company-tag">Company: </span>    
